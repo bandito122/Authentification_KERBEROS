eb5f5f142389e18da6a574794e0c818515e61b40
Julien <ditroia.j@hotmail.com>
2017-03-08 16:59:05 +0100

    commit initial: deux classes avec une méthode main() chacune: un KerberosAS et un KerberosTGS (bis)

---
+++ src/main/KerberosAS.java
@@ -0,0 +1,15 @@
+package main;
+
+/*
+ * @author Julien
+ */
+public class KerberosAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/KerberosTGS.java
@@ -0,0 +1,14 @@
+package main;
+
+/*
+ * @author Julien
+ */
+public class KerberosTGS {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
